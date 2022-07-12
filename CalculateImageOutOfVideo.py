8e6ba3e5c43563bf75aa41d323be4a6467f8272c
Ricardo <ricagraca@hotmail.com>
2022-07-12 22:59:47 +0100

    Added progress bar

--- CalculateImageOutOfVideo.py
+++ CalculateImageOutOfVideo.py
@@ -1,10 +1,12 @@
+import os
+from os import path
 
 from CompressImage import CompressImage
 from BlockMapper import BlockMapper
 import cv2
 import numpy as np
 import math
-
+import click
 
 def number_of_frames(video_path):
     vidcap = cv2.VideoCapture(video_path)
@@ -18,19 +20,19 @@ def video_porportion(video_path):
     width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
     return height, width
 
-    
-class CalculateImageOutOfVideo:
 
+class CalculateImageOutOfVideo:
     """
         Function that tells how similar two images are
         The bigger the value, the bigger the difference
     """
+
     def __init__(self, video_path, number, well_divided=False, skip=1):
         self.video_path = video_path
         self.number = number
         self.height, self.width = None, None
         print(number_of_frames(video_path))
-        self.skip = number_of_frames(video_path)//number if well_divided else skip
+        self.skip = number_of_frames(video_path) // number if well_divided else skip
 
     """
         From video, return a number of frames while
@@ -45,15 +47,14 @@ class CalculateImageOutOfVideo:
         success = True
 
         print("Getting video frames")
-        while success and counter//self.skip < self.number:
+        while success and counter // self.skip < self.number:
             success, img = vidcap.read()
-            self.height, self.width =  len(img),  len(img[0])
+            self.height, self.width = len(img), len(img[0])
             if counter % self.skip == 0:
                 selected_frames.append(img)
             counter += 1
 
         self.selected_frames = selected_frames
-
 
     def calculate_image(self, image, compare_algorithm, factorx, factory):
 
@@ -64,14 +65,13 @@ class CalculateImageOutOfVideo:
         # Compress image with factory and factorx
         reduced_image = CompressImage(image, compare_algorithm, factory, factorx).calculate()
         block_map = BlockMapper(reduced_image, compare_algorithm)
-        
+
         # Calculate map for image
         print('Calculating Map')
         for frame in self.selected_frames:
             block_map.check_image(frame)
-            
-        return self.create_image_out_of_mapping(image, block_map, compare_algorithm, factory, factorx)
 
+        return self.create_image_out_of_mapping(image, block_map, compare_algorithm, factory, factorx)
 
     def create_image_out_of_mapping(self, image, block_map, compare_algorithm, factory, factorx):
 
@@ -79,22 +79,27 @@ class CalculateImageOutOfVideo:
         width = len(image[0])
 
         # Create result image
-        image = np.ndarray(shape=(height,width,3))
+        image = np.ndarray(shape=(height, width, 3))
 
         # Create Image out of mapping
         print('Creating image out of map')
         print(factorx, factory, len(block_map.map))
-        for pos in block_map.map:
-            img, length = block_map.map[pos]
-            img_height, img_width = len(img), len(img[0])
-            block_height, block_width = int(math.ceil(img_height/factory)), int(math.ceil(img_width/factorx))
-            reduced_image = CompressImage(img, compare_algorithm, block_width, block_height).calculate()
-            for f in range(len(reduced_image)):
-                y = min(pos[0] * factory + f, height-1)
-                x1 = pos[1] * factorx
-                x2 = min(x1 + len(reduced_image[f]),len(image[y]))
-                # print(y,x1,x2, factorx, factory)
-                image[y][x1:x2] = reduced_image[f][:x2-x1]
+
+        positions = [pos for pos in block_map.map]
+
+        with click.progressbar(range(len(positions))) as bar:
+            for pos in bar:
+                pos = positions[pos]
+                img, length = block_map.map[pos]
+                img_height, img_width = len(img), len(img[0])
+                block_height, block_width = int(math.ceil(img_height / factory)), int(math.ceil(img_width / factorx))
+                reduced_image = CompressImage(img, compare_algorithm, block_width, block_height).calculate()
+                for f in range(len(reduced_image)):
+                    y = min(pos[0] * factory + f, height - 1)
+                    x1 = pos[1] * factorx
+                    x2 = min(x1 + len(reduced_image[f]), len(image[y]))
+                    # print(y,x1,x2, factorx, factory)
+                    image[y][x1:x2] = reduced_image[f][:x2 - x1]
 
         print("Created image successfully")
         self.calculated_image = image
@@ -103,4 +108,7 @@ class CalculateImageOutOfVideo:
     def save_image(self, file_name):
         # assert self.calculated_image != None
         print("Saved image in " + file_name)
+        folder = '/'.join(file_name.split('/')[:-1])
+        if not path.exists(folder):
+            os.mkdir(folder)
         cv2.imwrite(file_name, self.calculated_image)  # save frame as JPEG file
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from email.mime import image
 import imageio
 import scipy as scipy
@@ -14,20 +13,19 @@ VIDEO_FOLDER = 'video/'
 SAVED_FOLDER = 'saved/'
 
 
-def create(image_file, image_calculator=None, frame_number=100, video_file="", factorx=10, factory=10, output=None):
-
+def create_image(image_file, image_calculator=None, frame_number=100, video_file="", factorx=10, factory=10, output=None):
     if image_calculator is None and video_file == "":
         raise ValueError("expectted an image calculator or a video file")
 
-    image_calculator =  CalculateImageOutOfVideo(VIDEO_FOLDER + video_file, frame_number, well_divided=True) if (image_calculator is None) else image_calculator 
-    image = imageio.imread(IMAGE_FOLDER + image_file, pilmode='RGB')
-    image_calculator.calculate_image(image,CompareAverage(), factorx, factory)
+    image_calculator = CalculateImageOutOfVideo(video_file, frame_number, well_divided=True) if (
+                image_calculator is None) else image_calculator
+    image = imageio.imread(image_file, pilmode='RGB')
+    image_calculator.calculate_image(image, CompareAverage(), factorx, factory)
     image_calculator.save_image(SAVED_FOLDER + 'saved_' + (output if output is not None else image_file))
     return image_calculator
 
 
 def create_several(image_file, video_file, frame_number, number_images):
-
     image_file_name_without_extension = image_file.split(".")[0]
     video_file_name_without_extension = video_file.split(".")[0]
 
@@ -39,25 +37,25 @@ def create_several(image_file, video_file, frame_number, number_images):
 
     image_calculator = CalculateImageOutOfVideo(VIDEO_FOLDER + video_file, frame_number, well_divided=True)
     image = imageio.imread(IMAGE_FOLDER + image_file, pilmode='RGB')
-    
+
     if not path.exists(saving_directory):
         os.mkdir(saving_directory)
 
-    for i in range(1,number_images+1):
+    for i in range(1, number_images + 1):
         saved_file_name = image_file_name_without_extension + str(i) + '.' + extension
         if path.exists(saving_directory + saved_file_name):
             print("File " + saving_directory + saved_file_name + " exists.")
             continue
 
-        factory_, factorx_ = height//(i), width//(i)
+        factory_, factorx_ = height // (i), width // (i)
         image_calculator.calculate_image(image, CompareAverage(), factorx_, factory_)
         image_calculator.save_image(saving_directory + saved_file_name)
 
 
-def video_out_of_video(video1, video2, frame_number, number=10, factorx=40, factory=40, file_path="output.mp4", skip=None):
-
-    # Video input 
-    image_calculator =  CalculateImageOutOfVideo(video1, frame_number, well_divided=True) 
+def video_out_of_video(video1, video2, frame_number, number=10, factorx=40, factory=40, file_path="output.mp4",
+                       skip=None):
+    # Video input
+    image_calculator = CalculateImageOutOfVideo(video1, frame_number, well_divided=True)
 
     # Video for output
     vidcap = cv2.VideoCapture(video2)
@@ -66,7 +64,7 @@ def video_out_of_video(video1, video2, frame_number, number=10, factorx=40, fact
 
     # Create output video writer
     h, w = video_porportion(video1)
-    
+
     if h is None or w is None:
         raise ValueError("height or width are None")
 
@@ -74,32 +72,35 @@ def video_out_of_video(video1, video2, frame_number, number=10, factorx=40, fact
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     writer = cv2.VideoWriter(file_path, fourcc, fps, (w, h))
 
-    skip = (number_of_frames(video_path=video2)//number) if skip is None else skip   
+    skip = (number_of_frames(video_path=video2) // number) if skip is None else skip
 
     print("Creating video")
-    while success and counter//skip < number:
+    while success and counter // skip < number:
         # Get image from video for output
         success, img = vidcap.read()
         counter += 1
         if counter % skip != 0:
             continue
-        
-        print("Creating frame ", counter//skip)
+
+        print("Creating frame ", counter // skip)
         # Calculate image out of the input video
         calculated_image = image_calculator.calculate_image(img, CompareAverage(), factorx=factorx, factory=factory)
-        
+
         # Write image to output 
         writer.write(calculated_image)
 
     writer.release()
 
 
+# Create image
+if __name__ == "__main__":
+    create_image(image_file="image/me.jpg", video_file="video/pickle_rick.mp4", frame_number=5, factorx=25, factory=25)
 
-
+"""
 if __name__ == '__main__':
-
     video_1 = VIDEO_FOLDER + 'pickle_rick.mp4'
     video_2 = VIDEO_FOLDER + 'stay_with_me.mp4'
     frame_number = 2
-    
+
     video_out_of_video(video_1, video_2, frame_number)
+"""
